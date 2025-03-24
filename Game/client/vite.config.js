b4263b9b7b40de086ee198325438a1028a086162
chri-m <chrismaharjan.412@gmail.com>
2025-03-24 21:22:27 +0545

    Update vite.config.js

--- Game/client/vite.config.js
+++ Game/client/vite.config.js
@@ -3,11 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Adjust this base if your app is deployed under a subfolder
   plugins: [react()],
   esbuild: {
     loader: 'jsx', // This will tell esbuild to handle .jsx files correctly
   },
   css: {
     postcss: './postcss.config.cjs',
+  },
+  build: {
+    outDir: 'dist', // This ensures output goes into the dist folder
   },
 })
